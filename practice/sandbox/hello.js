bbbeaf37ed298f2cc06bd445d82533c0f56aa197
Pei H <w0472206@nscc.ca>
2023-09-10 15:08:16 -0300

    practice add hello file

---
+++ practice/sandbox/hello.js
@@ -0,0 +1,13 @@
+"use strict";  //do good javascript code
+
+function main()
+{
+    var name = "Pei";
+    console.log("Hello, " + name + "!!");
+}
+
+//not modify code below; use to trigger code above
+if (require.main === module)
+{
+    main();
+}
