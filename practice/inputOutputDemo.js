c3df08d5dcb70976b475d88d2acd4ab643b7026c
Pei H <w0472206@nscc.ca>
2023-09-11 00:07:11 -0300

    learned input and output

---
+++ practice/inputOutputDemo.js
@@ -0,0 +1,20 @@
+"use strict";  //do good javascript code
+var readlineSync = require("readline-sync");
+function main()
+{
+    //print one line
+   console.log("Meatloag and Peanut are so cute!\t\thello piggies");
+   console.log("hello piggy");
+
+   //input section
+   var name = readlineSync.question("What is your name?");
+
+   //output section
+   console.log("Hello, " + name + "!")
+}
+
+//not modify code below; use to trigger code above
+if (require.main === module)
+{
+    main();
+}
---
+++ practice/sandbox/codeTemplate.js
@@ -0,0 +1,12 @@
+"use strict";  //do good javascript code
+
+function main()
+{
+   //enter your code
+}
+
+//not modify code below; use to trigger code above
+if (require.main === module)
+{
+    main();
+}
